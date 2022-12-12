70f8fb5e30ea63e2100ced83297921722f25894e
Corneliu <corneliu.rotari99@gmail.com>
2022-12-12 05:41:01 +0200

    Added io package

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import io.Output;
+import io.Input;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class Main {
+    private Main() {
+    }
+
+    /**
+     * Entry point in the system
+     * @param args - command line arguments
+     */
+    public static void main(final String[] args) {
+        Output output = new Output();
+        Input input = new Input(args);
 
     }
 }
---
+++ src/io/Input.java
@@ -0,0 +1,11 @@
+package io;
+
+public final class Input {
+    /**
+     * Initializes the input
+     * @param args - cli arguments
+     */
+    public Input(String[] args) {
+
+    }
+}
