35d4c018415fc878208669960fff208d51c0cac5
Corneliu <corneliu.rotari99@gmail.com>
2023-01-31 20:35:10 +0200

    Add oop subfolder

--- card-game-backend/src/implementation/utils/constants/EnvironmentCardConstants.java
+++
@@ -1,7 +0,0 @@
-package implementation.utils.constants;
-
-public interface EnvironmentCardConstants {
-    String FIRESTORM = "Firestorm";
-    String HEART_HOUND = "Heart Hound";
-    String WINTERFELL = "Winterfell";
-}
--- streaming-service-backend/src/components/user/account/Premium.java
+++
@@ -1,7 +0,0 @@
-package components.user.account;
-
-public interface Premium {
-    int PRICE = 10;
-    int NR_FREE_MOVIES = 15;
-    String TYPE = "premium";
-}
