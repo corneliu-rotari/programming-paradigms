59706bfce4693d473df7084a5ef05c71bcb709e2
Rotari Corneliu <corneliu.rotari99@gmail.com>
2023-05-25 09:16:57 +0300

    [Lambda-Calculus] Refactro some lines in checker

--- functional-programming/Lambda-Calculus-Interpreter/check.sh
+++ functional-programming/Lambda-Calculus-Interpreter/check.sh
@@ -6,6 +6,6 @@ while read -r line; do
 	aux=$(echo "$line" | sed 's/points//g')
 	num=$(echo "$aux" | cut -c2-)
 	sum=$(($sum + $num))
-done < <(runhaskell test.hs | grep '+')
+done < <(stack runhaskell test.hs | grep '+')
 
 echo "Grade: $sum/150"
